f857ab3298a9b7f6c288a0d65ed5c706a6dc6410
Nickolay Svetlov <ns@idaproject.com>
2019-09-13 14:12:08 +0300

    Achievements components

---
+++ components/achievements/AchievementCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="achievement-card">
+        <div class="achievement-card__name">Название достижение</div>
+
+        <div class="achievement-card__desc">
+            Очень длинное название достижения. Тут описано какой ценой игрок заполучил данный трофей
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AchievementCard'
+    };
+</script>
+
+<style lang="scss">
+    .achievement-card {
+        $block: &;
+        position: relative;
+        display: block;
+        width: 240px;
+        height: 180px;
+        padding: 15px 24px;
+        border-radius: 4px;
+        background-color: $c-main;
+        box-shadow: 0 1px 5px rgba(#000, .11);
+        color: #fff;
+
+        &__name {
+            font-size: 18px;
+            line-height: 1.4;
+        }
+
+        &__desc {
+            font-size: 14px;
+            line-height: 1.6;
+
+            &:not(:first-child) {
+                margin-top: 1em;
+            }
+        }
+    }
+</style>
---
+++ components/achievements/AchievementsList.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul class="achievements-list">
+        <li class="achievements-list__item">
+            <achievement-card/>
+        </li>
+
+        <li class="achievements-list__item">
+            <achievement-card/>
+        </li>
+
+        <li class="achievements-list__item">
+            <achievement-card/>
+        </li>
+
+        <li class="achievements-list__item">
+            <achievement-card/>
+        </li>
+    </ul>
+</template>
+
+<script>
+    import AchievementCard from './AchievementCard';
+
+    export default {
+        name: 'AchievementsList',
+        components: {AchievementCard}
+    };
+</script>
+
+<style lang="scss">
+    .achievements-list {
+        display: flex;
+        flex-wrap: wrap;
+        margin: -10px -20px;
+        list-style: none;
+        padding: 0;
+
+        &__item {
+            margin: 10px 20px;
+        }
+    }
+</style>
